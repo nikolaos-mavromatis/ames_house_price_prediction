6d5139e8edccab0b1a512655574d893f6f334d18
Nikolaos Mavromatis <nck.mavromatis@gmail.com>
2024-09-25 12:15:11 +0200

    wrapped up all steps in a pipeline

---
+++ ames_house_price_prediction/engine.py
@@ -0,0 +1,22 @@
+import typer
+from loguru import logger
+
+from ames_house_price_prediction.data import prepare_data
+from ames_house_price_prediction.features import make_features
+from ames_house_price_prediction.modeling import train_model, make_predictions
+
+app = typer.Typer()
+
+
+@app.command()
+def engine():
+    logger.info("Running pipeline...")
+    prepare_data()
+    make_features()
+    train_model()
+    make_predictions()
+    logger.success("Pipeline ran successfully.")
+
+
+if __name__ == "__main__":
+    app()
