36a7488dcd9d1bfb8cb39f79417d0ad61f931852
Nikolaos Mavromatis <nck.mavromatis@gmail.com>
2024-11-01 20:30:39 +0100

    added ordinal features pipeline step

--- ames_house_price_prediction/features/features.py
+++ ames_house_price_prediction/features/features.py
@@ -1,16 +1,27 @@
 from pathlib import Path
 
+import numpy as np
 import typer
 from loguru import logger
 from tqdm import tqdm
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, OrdinalEncoder, RobustScaler
 from pickle import dump
 
-from ames_house_price_prediction.config import MODELS_DIR, PROCESSED_DATA_DIR, TARGET
-from ames_house_price_prediction.features.utils import calculate_lot_age
+from ames_house_price_prediction.config import (
+    CAT_FEATURES,
+    MODELS_DIR,
+    NUM_FEATURES,
+    ORD_FEATURES,
+    PROCESSED_DATA_DIR,
+    TARGET,
+)
+from ames_house_price_prediction.features.utils import (
+    calculate_lot_age,
+    calculate_years_since_remodel,
+)
 
 app = typer.Typer()
 
@@ -25,22 +36,33 @@ def main(
     logger.info("Generating features from dataset...")
     input_df = pd.read_parquet(input_path)
 
-    df = input_df.pipe(calculate_lot_age)
+    df = input_df.pipe(calculate_lot_age).pipe(calculate_years_since_remodel)  # REFACTOR
 
-    print(df.head())
-
-    numeric_features = ["LotArea", "LotAge", "OverallQual", "OverallCond"]
-    numeric_transformer = Pipeline(steps=[("scaler", MinMaxScaler())])
-
-    categorical_features = ["Neighborhood", "SaleCondition"]
+    numeric_transformer = Pipeline(steps=[("scaler", RobustScaler())])
+    ordinal_transformer = Pipeline(
+        steps=[
+            (
+                "ord_encoding",
+                OrdinalEncoder(
+                    dtype=int,
+                    categories=len(ORD_FEATURES) * [["Po", "Fa", "TA", "Gd", "Ex"]],
+                    handle_unknown="use_encoded_value",
+                    unknown_value=-1,
+                ),
+            )
+        ]
+    )
     categorical_transformer = Pipeline(
-        steps=[("encoding", OneHotEncoder(drop="first", sparse_output=False))]
+        steps=[
+            ("oh_encoding", OneHotEncoder(drop="first", sparse_output=False)),
+        ]
     )
 
     preprocessor = ColumnTransformer(
         transformers=[
-            ("num", numeric_transformer, numeric_features),
-            ("cat", categorical_transformer, categorical_features),
+            ("num", numeric_transformer, NUM_FEATURES),
+            ("ord", ordinal_transformer, ORD_FEATURES),
+            ("cat", categorical_transformer, CAT_FEATURES),
         ],
         remainder="drop",
         verbose_feature_names_out=False,
@@ -48,7 +70,6 @@ def main(
     transformed_df = pd.DataFrame(
         preprocessor.fit_transform(df), columns=preprocessor.get_feature_names_out()
     )
-    print(transformed_df.head())
 
     with open(preprocessor_path, "wb") as f:
         dump(preprocessor, f, protocol=5)
