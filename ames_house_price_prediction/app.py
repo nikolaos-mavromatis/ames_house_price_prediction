b6913daed831f6c263ce6eba1a41cd06e2fd366c
Nikolaos Mavromatis <nck.mavromatis@gmail.com>
2024-11-01 21:44:43 +0100

    separated concerns; replaced built-in inference with api call

--- ames_house_price_prediction/app.py
+++ ames_house_price_prediction/app.py
@@ -1,32 +1,10 @@
-import datetime
 import locale
-from pathlib import Path
-import pickle
+import requests
 
-import pandas as pd
 import streamlit as st
 
-from ames_house_price_prediction.config import MODELS_DIR
-from ames_house_price_prediction.features.utils import (
-    calculate_lot_age,
-    calculate_years_since_remodel,
-)
 
 locale.setlocale(locale.LC_ALL, "")
-
-
-@st.cache_resource
-def load_model(path: Path):
-    with open(path, "rb") as f:
-        model = pickle.load(f)
-    return model
-
-
-@st.cache_resource
-def load_preprocessor(path: Path):
-    with open(path, "rb") as f:
-        preprocessor = pickle.load(f)
-    return preprocessor
 
 
 main, help = st.tabs(["Main", "Help"])
@@ -56,7 +34,12 @@ with main:
                 step=1,
                 help="Select the quality of the materials.",
             )
-            remodeled_val = st.checkbox("My house has been remodeled.", value=False)
+            remodeled_year_val = st.number_input(
+                "Remodeled Year",
+                value=None,
+                step=1,
+                help="Leave empty if the house has not been remodeled.",
+            )
 
         with col2:
             area_val = st.number_input(
@@ -71,50 +54,28 @@ with main:
                 step=1,
                 help="Select the condition of the house.",
             )
-            remodeled_year_val = st.number_input(
-                "Remodeled Year",
-                value=None,
-                step=1,
-                help="Leave empty if the house has not been remodeled.",
-            )
 
         # Every form must have a submit button.
         _, col, _ = st.columns(3)
         with col:
             submitted = st.form_submit_button("Quote me now!", use_container_width=True)
         if submitted:
-            # STEPS:
-            # 1. make inference dataframe
-            # 2. load preprocessor & preprocess data
-            # 3. load model & predict
-
-            # 1. make inference dataframe
-            answers = {
+            # collect all form inputs to pass into the api call
+            params = {
                 "LotArea": area_val,
                 "YearBuilt": build_year_val,
-                "YearRemodAdd": remodeled_year_val,
-                "YrSold": datetime.datetime.now().year,
+                "YearRemodAdd": remodeled_year_val if remodeled_year_val else -1,
                 "OverallQual": qual_val,
                 "OverallCond": cond_val,
             }
-            inference_data = (
-                pd.DataFrame(answers, index=[0])
-                .pipe(calculate_lot_age)
-                .pipe(calculate_years_since_remodel)
-            )
-
-            # 2. load preprocessor & preprocess data
-            preprocessor = load_preprocessor(MODELS_DIR / "preprocessor.pkl")
-            preprocessed_inference_data = preprocessor.transform(inference_data)
-
-            # 3. load model & predict
-            model = load_model(MODELS_DIR / "model.pkl")
-            quote = model.predict(preprocessed_inference_data)
-            quote_in_dollars = locale.currency(quote[0][0], grouping=True)
-            print(quote)
+            quote = requests.get(
+                # FIXME: replace local host+port with cloud details
+                url="http://localhost:8000/quote/",
+                params=params,
+            ).json()
+            quote_in_dollars = locale.currency(quote, grouping=True)
 
             st.divider()
-
             st.subheader(
                 f"The value of your house is estimated at :blue[**{quote_in_dollars}**]. "
             )
