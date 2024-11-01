786b34bcb3aa9f85439e0b0ff7e423a8e51565e1
Nikolaos Mavromatis <nck.mavromatis@gmail.com>
2024-11-01 20:28:45 +0100

    added years since remodel method

--- ames_house_price_prediction/features/utils.py
+++ ames_house_price_prediction/features/utils.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import numpy as np
 
 
 def calculate_lot_age(df: pd.DataFrame) -> pd.DataFrame:
     """Calculate the difference between the year a house was built and the year it was sold."""
     return df.assign(LotAge=lambda x: x["YrSold"] - x["YearBuilt"])
+
+
+def calculate_years_since_remodel(df: pd.DataFrame) -> pd.DataFrame:
+    """Calculate the difference between the year a house was sold and the year it was remodelled.
+
+    -1 when the property has never been remodelled.
+    """
+    return df.assign(
+        YearsSinceRemod=lambda x: np.where(
+            x["YearRemodAdd"] > x["YearBuilt"], x["YrSold"] - x["YearRemodAdd"], -1
+        ),
+    )
